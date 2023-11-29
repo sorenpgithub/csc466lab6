2fa2fd867824b988317f6d0c2bf45224f05f354a
Othilia <74982948+Othilia@users.noreply.github.com>
2023-11-29 13:13:46 -0800

    Update classfierEvaluation.py

--- classfierEvaluation.py
+++ classfierEvaluation.py
@@ -2,19 +2,37 @@ from functions import *
 #classifierEvaluation <pred.csv> <gt.csv> [-s (silent, only outputs total metrics)] [-w (write output)]
 
 """
-for each author, need num correct predicted, false positives, and documents written by author not attributed to them, need precision, recall and f1 measure
+for each author, need num correct predicted (TP), false positives, 
+and (num?) documents written by author not attributed to them (--> TN?)
+need precision, recall and f1 measure
 
 also need overall values of above
 """
 def generate_mets(cm):
+
+    #From https://stackoverflow.com/questions/31324218/scikit-learn-how-to-obtain-true-positive-true-negative-false-positive-and-fal
+    #switched the axis though, because the stackoverflow case was based on that predictions were the colomns
+    #and our confusion matrix has prediction as row
+    FP = cm.sum(axis=1) - np.diag(cm)  
+    FN = cm.sum(axis=0) - np.diag(cm)
+    TP = np.diag(cm)
+    TN = cm.values.sum() - (FP + FN + TP)
+
+    precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+    recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+    f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+    accuracy = (TP + TN) / (TP + TN + FP + FN) if (TP + TN + FP + FN) > 0 else 0
+
+    mets = []
+    #for author in len(cm):
     pass
-
-
+    #return TP, FP, TN, precision, recall, f1
 """
 Mets [AUTHOR1(tp, fp, misses, precision, recall, f1), AUTHOR2(...)..., TOTAL(tp,..f1)]
 if silent only print last one
 """
 def output_mets(mets, silent):
+    #print("mets:", mets)
     pass
 
 def main():
